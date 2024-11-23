c2f78f8491cdcf0bfa17c74954457d5e287d2e3e
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-23 16:27:06 +0100

    Work on review.html page
    Added structure and styling for review.html page.
    Added JS functionality to process GET request
    from index.html form (no backend).

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 'use strict';
 
-// const labels = document.querySelectorAll('label');
+const btn = document.querySelector('#btnSubmit');
+const avatar = document.querySelector('#avatarPic');
+const nm = document.querySelector('#profileName');
+const occ = document.querySelector('#profileOccupation');
+const msg = document.querySelector('#profileMessage');
 
-// labels.forEach(label => {
-//   label.style.textShadow = '2px 2px';
-// });
+function getForm(input) {
+  let result = null,
+    tmp = [];
+  location.search
+    .slice(1)
+    .split('&')
+    .forEach(function (item) {
+      tmp = item.split('=');
+      if (tmp[0] === input) result = decodeURIComponent(tmp[1]);
+    });
+  result = result.split('+').join(' ');
+  return result;
+}
+
+const updateReview = function () {
+  nm.textContent = getForm('name');
+  occ.textContent = getForm('occupation');
+  msg.textContent = getForm('message');
+};
+
+updateReview();
